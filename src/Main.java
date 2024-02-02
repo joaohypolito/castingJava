6e4419c8740c8151ca3c0949af92ef28edb0c006
João Pedro A. Hypólito <jpazhy@gmail.com>
2024-02-02 13:11:16 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+// Declare uma variável do tipo double e uma variável do tipo int. Faça o casting da variável double para int e imprima o resultado.
+public class Main {
+
+    public static void main(String[] args) {
+
+        double numberDouble = 13.5;
+        int numberInt = (int)numberDouble; // O casting em Java é a conversão de um tipo de dado para outro.
+
+        System.out.println("Número double: " + numberDouble);
+        System.out.println("Número inteiro: " + numberInt);
+
+    }
+    
+}
